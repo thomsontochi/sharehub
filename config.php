89f0d28792f2bd57706ada71a1d1b824a344db02
Opiaaustin <38527343+thomsontochi@users.noreply.github.com>
2018-12-07 23:00:57 +0100

    this is an object oriented programming web site
    the website is simple , i did not focus on the user interface, the main aim was to make an mvc web site where shares can be posted.

---
+++ config.php
@@ -0,0 +1,11 @@
+<?php
+
+// Define DB Params
+define("DB_HOST", "localhost");
+define("DB_USER", "root");
+define("DB_PASS", "");
+define("DB_NAME", "sharehub");
+
+// Define URL
+define("ROOT_PATH", "/sharehub/");
+define("ROOT_URL", "http://localhost/sharehub/"); 
